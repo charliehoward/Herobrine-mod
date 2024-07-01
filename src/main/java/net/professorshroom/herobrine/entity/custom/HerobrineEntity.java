3b33fa44009ff3479b2b12970e30d8974d4599ef
Charlie Howard <charlie@howardgb.com>
2024-07-01 19:42:55 +0100

    Update HerobrineEntity.java

--- src/main/java/net/professorshroom/herobrine/entity/custom/HerobrineEntity.java
+++ src/main/java/net/professorshroom/herobrine/entity/custom/HerobrineEntity.java
@@ -1,6 +1,10 @@
 package net.professorshroom.herobrine.entity.custom;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.FleeEntityGoal;
+import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
@@ -8,10 +12,13 @@ import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.professorshroom.herobrine.Herobrine;
 import net.professorshroom.herobrine.entity.ModEntities;
 import org.jetbrains.annotations.Nullable;
@@ -24,15 +31,16 @@ public class HerobrineEntity extends AnimalEntity {
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-
-        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 25f));
+        this.goalSelector.add(2, new LookAtEntityGoal(this, PlayerEntity.class, 100f));
+        this.goalSelector.add(1, new HerobrineEntity.FleeGoal(this, PlayerEntity.class, 25.0F, 5, 5));
     }
 
 
     public static DefaultAttributeContainer.Builder createHerobrineAttributes() {
         return MobEntity.createMobAttributes()
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 10)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 5);
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 5)
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 1);
+
     }
 
     @Override
@@ -50,6 +58,30 @@ public class HerobrineEntity extends AnimalEntity {
 
         Herobrine.LOGGER.info("Registering Entities for " + Herobrine.MOD_ID);
 
+    }
+
+    static class FleeGoal<T extends LivingEntity> extends FleeEntityGoal<T> {
+        private final HerobrineEntity herobrine;
+
+        public FleeGoal(HerobrineEntity herobrine, Class<T> fleeFromType, float distance, double slowSpeed, double fastSpeed) {
+            super(herobrine, fleeFromType, distance, slowSpeed, fastSpeed);
+            this.herobrine = herobrine;
+        }
+    }
+
+    @Override
+    public boolean canWalkOnFluid(FluidState state) {
+        return super.canWalkOnFluid(state);
+    }
+
+    @Override
+    public boolean canImmediatelyDespawn(double distanceSquared) {
+        return super.canImmediatelyDespawn(distanceSquared);
+    }
+
+    @Override
+    public void setDespawnCounter(int despawnCounter) {
+        super.setDespawnCounter(1);
     }
 
 }
